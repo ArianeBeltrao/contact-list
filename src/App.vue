6faddefecf9127581cc9aacd6369cd92f76b2648
Ariane <ariane@globalfy.com>
2025-02-12 15:01:06 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Button } from './components/ui/button'
+</script>
+
+<template>
+  <div class="px-3 py-4 flex items-center justify-between w-full bg-blue-300">
+    Contact list test
+  </div>
+  <div>
+    <Button>Click me test</Button>
+    <p class="text-green-300 bg-yellow-200">Hello Vite test!</p>
+  </div>
+</template>
+
+<style src="./assets/tailwind.css" />
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from "node:url";
+import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@tailwindcss/postcss";
+
+// https://vite.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
